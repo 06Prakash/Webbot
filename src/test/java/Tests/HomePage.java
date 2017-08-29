900ee76e651c7e9698a8f604cd06382b57f31ba3
panusalini <panusalini@016871.corp.athenahealth.com>
2017-08-29 11:37:23 +0530

    Chrome instance creation in test script

--- src/test/java/Tests/HomePage.java
+++ src/test/java/Tests/HomePage.java
@@ -1,48 +1,49 @@
 package Tests;
 
 import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 //
-//import org.apache.log4j.LogManager;
-//import org.apache.log4j.Logger;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-//import junit.framework.Assert;
+import junit.framework.Assert;
 import PageObject.LandingPage;
 import PageObject.SearchResultPage;
 import ui_automation.hawk.TestBase;
 
-
 public class HomePage extends TestBase {
-//	public static Logger log = LogManager.getLogger(HomePage.class.getName());
+	public static Logger log = LogManager.getLogger(HomePage.class.getName());
 
+	protected WebDriver driver;
 
 	@BeforeTest
-	public void initialize() throws IOException
-	{
-		 driver =initializeDriver();
-		 navigate_to_url(prop.getProperty("url"));
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+		navigate_to_url("http://google.com");
 	}
-	
+
 	@Test(dataProvider = "getData")
 	public void basePageNavigation(String searchName, String testIdentity) throws IOException {
 		LandingPage lp = new LandingPage(driver);
-//		log.info("Reached the google search page");
+		log.info("Reached the google search page");
 		System.out.println(searchName);
 		System.out.println(testIdentity);
 		lp.getSearchBox().sendKeys(searchName);
-		// k.sendKeys(searchName);
 		lp.getSearchBox().sendKeys(Keys.ENTER);
 		// Sending the control to the search result page
 		SearchResultPage srp = new SearchResultPage(driver);
 		String rhsDivText = srp.getResultDiv().getText();
 		boolean text = rhsDivText.contains(searchName);
 		System.out.println(text);
-//		Assert.assertEquals("Ensured that the search returned the expected result", true, text);
-//		log.info("Searched the text as expected");
+		Assert.assertEquals("Ensured that the search returned the expected result", true, text);
+		log.info("Searched the text as expected");
 	}
 
 	@DataProvider
@@ -51,13 +52,9 @@ public class HomePage extends TestBase {
 		data[0][0] = prop.getProperty("searchName");
 		data[0][1] = "Test 1";
 
-		// data[1][0] = "https://www.google.co.in";
-		// data[1][1] = "Watir";
-		// data[1][2] = "Test 2";
-
 		return data;
 	}
-	
+
 	@AfterTest
 	public void teardown() throws IOException {
 		// Take screen shots
--- src/test/java/Tests/SikuliTest.java
+++ src/test/java/Tests/SikuliTest.java
@@ -2,6 +2,9 @@ package Tests;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,31 +14,31 @@ import PageObject.IPhoneHome;
 import junit.framework.Assert;
 import ui_automation.hawk.TestBase;
 
-
 public class SikuliTest extends TestBase {
-//	public static Logger log = LogManager.getLogger(HomePage.class.getName());
+	public static Logger log = LogManager.getLogger(SikuliTest.class.getName());
 
+	protected WebDriver driver;
 
 	@BeforeTest
-	public void initialize() throws IOException
-	{
-		 driver = initializeDriver();
-		 navigate_to_url("https://www.apple.com/in/iphone/");
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+		navigate_to_url("https://www.apple.com/in/iphone/");
 	}
-	
+
 	@Test
 	public void basePageNavigation() throws IOException, InterruptedException {
 		IPhoneHome iphonehome = new IPhoneHome(driver);
 
 		Assert.assertEquals("iPhone", iphonehome.getPageHeader());
-		// click the iphone6 image		
+		// click the iphone6 image
+		log.info("Ensured that the next page is reached and the image button got process and found as expected");
 		iphonehome.clickImage(iphonehome.iphoneimg);
 	}
-	
+
 	@AfterTest
 	public void teardown() throws IOException {
 		// Take screen shots
-	
+
 		driver.close();
 	}
 }
